6a38f79cec72581be34e4825d36fbf0650bf21af
ElenaVansovich <elena.vansovich.96@mail.ru>
2016-12-29 13:13:25 +0300

    Linux coding style

--- hello_timer.c
+++ hello_timer.c
@@ -3,8 +3,11 @@
 #include <linux/device.h>
 #include <linux/timer.h>
 #include <linux/fs.h>
+
 #define DELAY 3
-#define TEXT "Hello, world!"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("ELENA_VANSOVICH");
 
 static unsigned long times = 0;
 static unsigned char timer_exists = 0;
@@ -12,36 +15,33 @@ static unsigned char timer_exists = 0;
 static int timer_init(void);
 static void timer_exit(void);
 
-static ssize_t times_show(struct kobject *, struct kobj_attribute *, char *);
-static ssize_t times_store(struct kobject *, struct kobj_attribute *, const char *, size_t);
+static ssize_t show_time(struct kobject *, struct kobj_attribute *, char *);
+static ssize_t store_time(struct kobject *, struct kobj_attribute *, const char *, size_t);
 
 static void repeat(unsigned long);
 
-
 static struct kobj_attribute times_attrb =
-    __ATTR(interval, 0664, times_show, times_store);
+    __ATTR(interval, 0664, show_time, store_time);
 
 static struct kobject *times_obj = NULL;
 
 static struct timer_list timer;
 
 
-static ssize_t times_show(struct kobject *kobj, struct kobj_attribute *attr,
+static ssize_t show_time(struct kobject *kobj, struct kobj_attribute *attr,
                           char *buf)
 {
     	return sprintf(buf, "%lu\n", times);
 }
 
-static ssize_t times_store(struct kobject *kobj, struct kobj_attribute *attr,
+static ssize_t store_time(struct kobject *kobj, struct kobj_attribute *attr,
                            const char *buf, size_t count)
 {
-    	if (kstrtoul(buf, 10, &times) == -EINVAL) 
-	{
+    	if (kstrtoul(buf, 10, &times) == -EINVAL) {
         	return -EINVAL;
     	}
 
-    	if (timer_exists) 
-	{
+    	if (timer_exists) {
         	del_timer(&timer);
     	}
 
@@ -59,14 +59,12 @@ static void repeat(unsigned long arg)
 {
     	unsigned long i = 0;
 
-    	if (!arg)
-	{
+    	if (!arg) {
         	return;
     	}
 
-    	for (i = 0; i < arg; ++i) 
-	{
-        	printk(KERN_INFO "%s\n", TEXT);
+    	for (i = 0; i < arg; ++i) {
+        	printk(KERN_INFO "%s\n", "Hello, word!");
     	}
 
     	timer.expires = jiffies + DELAY * HZ;
@@ -76,17 +74,14 @@ static void repeat(unsigned long arg)
 
 static int __init timer_init()
 {
-
 	init_timer_on_stack(&timer);
 
 	times_obj = kobject_create_and_add("timer", NULL);
-	if (!times_obj)
-	{
+	if (!times_obj) {
 		return -ENOMEM;
 	}
 
-    	if (sysfs_create_file(times_obj, &times_attrb.attr))
-	{
+    	if (sysfs_create_file(times_obj, &times_attrb.attr)) {
         	timer_exit();
         	return -EINVAL;
     	}
@@ -96,19 +91,14 @@ static int __init timer_init()
 
 static void timer_exit()
 {
-	if (timer_exists)
-	{
+	if (timer_exists) {
 		del_timer(&timer);
 	}
 
-	if (times_obj)
-	{
+	if (times_obj) {
 		kobject_put(times_obj);
 	}
 }
 
 module_init(timer_init);
 module_exit(timer_exit);
-
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("ELENA_VANSOVICH");
